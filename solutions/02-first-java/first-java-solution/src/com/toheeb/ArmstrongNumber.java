9a5ef6a6692a048495032b4ee799704f3bc5a7d9
Toheeb Oyekola <toheeb.olawale.to23@gmail.com>
2022-11-22 16:46:36 +0100

    complete first-java solutions

---
+++ solutions/02-first-java/first-java-solution/src/com/toheeb/ArmstrongNumber.java
@@ -0,0 +1,38 @@
+package com.toheeb;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        System.out.println("Get Armstrong number");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter lower bound number: ");
+        int lowerBound = input.nextInt();
+        System.out.print("Please enter upper bound number: ");
+        int upperBound = input.nextInt();
+
+        for(int i = lowerBound; i < upperBound; i++){
+
+            int digit = 0;
+            int result = 0;
+            int originalNumber = i;
+
+            while(originalNumber != 0){
+                originalNumber /= 10;
+                ++digit;
+            }
+
+            originalNumber = i;
+
+            while(originalNumber != 0){
+                int remainder = originalNumber % 10;
+                result += Math.pow(remainder, digit);
+                originalNumber /= 10;
+            }
+
+            if(result == i){
+                System.out.print(i +", ");
+            }
+        }
+    }
+}
